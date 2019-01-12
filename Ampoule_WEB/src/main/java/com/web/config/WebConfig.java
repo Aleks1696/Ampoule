2677ff4573975ab613c33e5009bf918b66843e86
Антон <xanton.platovx@gmail.com>
2019-01-13 00:20:50 +0200

    Добавил WebConfig

--- Ampoule_WEB/src/main/java/com/web/config/WebConfig.java
+++ Ampoule_WEB/src/main/java/com/web/config/WebConfig.java
@@ -16,5 +16,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/js/**")
                 .addResourceLocations("classpath:/static/js/");
     }
+    //
 
 }
