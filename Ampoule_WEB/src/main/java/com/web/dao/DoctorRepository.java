a94b2f791e1671a2cac8079054d0501b2098a16c
Aleks1696 <aleks.borodavka@ukr.net>
2019-01-19 21:42:01 +0200

    добавил метод 'findByEmailAndPasswordEquals'

--- Ampoule_WEB/src/main/java/com/web/dao/DoctorRepository.java
+++ Ampoule_WEB/src/main/java/com/web/dao/DoctorRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface DoctorRepository  extends JpaRepository<Doctor, Long> {
     Doctor findByIdEquals(Long id);
 
+    Doctor findByEmailAndPasswordEquals (String email, String password);
+
 }
