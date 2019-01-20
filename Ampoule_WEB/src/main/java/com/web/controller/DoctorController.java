f23239f633fe1bc2ada1a81ae1094d9a9ec0ae4b
Aleks1696 <aleks.borodavka@ukr.net>
2019-01-20 21:02:53 +0200

    1) добавлен метод для получения всех карт 2) добавлен метод для автоматического заполнения данных о докторе на странице доктора

--- Ampoule_WEB/src/main/java/com/web/controller/DoctorController.java
+++ Ampoule_WEB/src/main/java/com/web/controller/DoctorController.java
@@ -7,7 +7,6 @@ import com.web.domain.Doctor;
 import com.web.domain.types.DoctorsType;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -15,7 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class DoctorController {
@@ -39,7 +39,17 @@ public class DoctorController {
         Doctor doctor = doctorRepository.findByEmailAndPasswordEquals(
                 request.getParameter("email"), request.getParameter("password")
         );
-        System.out.println(request.getDateHeader("email"));
+
+        //добавляем в сессию данные доктора
+        HttpSession session = request.getSession();
+        session.setAttribute("id", doctor.getId());
+        session.setAttribute("email", doctor.getEmail());
+        session.setAttribute("name", doctor.getName());
+        session.setAttribute("surname", doctor.getSurname());
+        session.setAttribute("doctorType", doctor.getDoctorsType());
+        session.setAttribute("phoneNumber", doctor.getPhoneNumber());
+        session.setAttribute("cards", doctor.getCards());
+
         return (doctor != null) ? true : false;
     }
 
@@ -72,6 +82,39 @@ public class DoctorController {
         jsonObject.put("birthday", card.getBirthday().toString());
         jsonObject.put("height", card.getHeight());
         jsonObject.put("weight", card.getWeight());
+        return jsonObject;
+    }
+
+    @RequestMapping(value = "/doctor/get/all/cards", method = RequestMethod.GET)
+    public @ResponseBody
+    JSONArray getAllCards() {
+        List<Card> cards = cardRepository.findAll();
+
+        JSONArray jsonArray = new JSONArray();
+        for (Card card : cards) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("id", card.getId());
+            jsonObject.put("name", card.getName());
+            jsonObject.put("surname", card.getSurname());
+            jsonObject.put("gender", card.getGender());
+            jsonObject.put("birthday", card.getBirthday());
+            jsonObject.put("height", card.getHeight());
+            jsonObject.put("weight", card.getWeight());
+            jsonArray.add(jsonObject);
+        }
+        return jsonArray;
+    }
+
+    @RequestMapping(value = "/doctor/get/active", method = RequestMethod.GET)
+    public @ResponseBody
+    JSONObject getCurrentDoctor(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        System.out.println(session);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", session.getAttribute("id"));
+        jsonObject.put("name", session.getAttribute("name"));
+        jsonObject.put("surname", session.getAttribute("surname"));
+
         return jsonObject;
     }
 
